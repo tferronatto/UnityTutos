53e02244d3e65ca1285afe0fff19c08de5aed0e4
hierronacho <tferronatto@gmail.com>
2017-12-12 23:45:12 -0400

    player animator and script

--- Roguelike/Assets/Scripts/GameManager.cs
+++ Roguelike/Assets/Scripts/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager : MonoBehaviour {
 
 	public static GameManager instance = null;
 	public BoardManager boardScript;
+	public int playerFoodPoints = 100;
+	[HideInInspector] public bool playersTurn = true;
 
 	private int level = 3;
 
@@ -25,7 +27,11 @@ public class GameManager : MonoBehaviour {
 	void InitGame(){
 		boardScript.SetupScene(level);
 	}
-	
+
+	public void GameOver(){
+		enabled = false;
+	}
+
 	// Update is called once per frame
 	void Update () {
 		
